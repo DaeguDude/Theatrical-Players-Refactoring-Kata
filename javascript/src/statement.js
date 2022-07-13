ac5600c70debf83712cc4fedd169f6418375a8ba
Daegudude <>
2022-07-13 09:05:47 +0900

    Extracting function

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -5,39 +5,18 @@ function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
+  const format = getFormatter();
 
   // NOTE: Smell: loop
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
 
-    // NOTE: Smell: switch statement
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    const thisAmount = calculateThisAmount(play, perf);
+
+    volumeCredits += calculateVolumeCredit(perf);
+    if ("comedy" === play.type)
+      volumeCredits += calculateExtraVolumeCredit(perf);
+
     // print line for this order
     result += ` ${play.name}: ${format(thisAmount / 100)} (${
       perf.audience
@@ -46,6 +25,45 @@ function statement(invoice, plays) {
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+
+function calculateVolumeCredit(perf) {
+  return Math.max(perf.audience - 30, 0);
+}
+
+function calculateExtraVolumeCredit(perf) {
+  return Math.floor(perf.audience / 5);
+}
+
+function getFormatter() {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+}
+
+function calculateThisAmount(play, perf) {
+  let result = 0;
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (perf.audience > 30) {
+        result += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (perf.audience > 20) {
+        result += 10000 + 500 * (perf.audience - 20);
+      }
+      result += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+
   return result;
 }
 
